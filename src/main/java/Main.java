5afac4826be58ad4b884888bdeebb642d435f0c6
maltebp <maltebp94@gmail.com>
2019-02-27 12:38:19 +0100

    Inserted port command method (probably won't be used)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,35 @@
+import java.util.regex.Pattern;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+
+    /** Converts an ip address and a portnumber into a correct port command,
+     *  in the format:
+     *      "PORT (h1,h2,h3,h4,p1,p2)
+     */
+    static String createPortCommand( String address, int portNumber ){
+
+        int p1 =  portNumber/256;
+        int p2 =  portNumber%256;
+
+        String[] ip = address.split( Pattern.quote("."));
+
+        String command = "PORT (";
+
+        for( String sub : ip ){
+            command += sub+",";
+        }
+
+        command += p1 + "," + p2+")";
+
+        return command;
+    }
+
 }
